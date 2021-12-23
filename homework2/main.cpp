c6f5350f1c9b1dd877e2ae1c3b4798d19b5c4e36
Maria Edel <77006511+mariaedel@users.noreply.github.com>
2021-12-23 13:53:17 +0300

    1

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -6,12 +6,7 @@
 
 using namespace std;
 
-template <typename T> 
-int sgn(T val) {
-    return (T(0) < val) - (val < T(0));
-}
-
-const float g = 9.81;
+const double g = 9.81;
 
 int main(int argc, char** argv)
 {
@@ -31,63 +26,67 @@ int main(int argc, char** argv)
         return 0;
     }
 
-
     ifstream file(input);
 
-    float h0;
+    double h0;
     file >> h0;
     
-    float vx0, vy0;
+    double vx0, vy0;
     file >> vx0 >> vy0;
 
-    float xi, hi;
-    vector<float> xis;
-    vector<float> his;
+    double xi, hi;
+    vector<double> X;
+    vector<double> H;
 
-    float y = h0, t = 0, x = 0.0, vx = vx0, vy = vy0;
+    double y = h0, t = 0, x = 0.0, vx = vx0, vy = vy0;
 
     int current_n = 0;
 
     while (file >> xi >> hi) {
-        xis.push_back(xi);
-        his.push_back(hi);       
+        X.push_back(xi);
+        H.push_back(hi);       
     }
 
     // finding current section
-    while (x > xis[current_n]) {
+    while (x > X[current_n]) {
         current_n++;
     }
 
     while (true) {
-        int dir = sgn(vx);
-        int next = (dir > 0) ? current_n : current_n - 1;
 
-        if (next < 0 || next >= xis.size()) {
+        int direction = vx / abs(vx);
+        int next_n;
+        
+        if (direction > 0) {
+            next_n = current_n;
+        } else {
+            next_n = current_n - 1;
+        }
+
+        if (next_n < 0 || next_n >= X.size()) {
             cout << current_n;
             return 0;
         }
 
-        float dt = (xis[next] - x) / vx;
+        double dt = (X[next_n] - x) / vx;
 
         y = y + vy * dt - g * dt * dt / 2;
 
-        if (y <= 0.0) {
+        if (y <= 0) {
             cout << current_n;
             return 0;
         } 
-        else if (y >= his[next]) {
-            x = xis[next];            
-            current_n += dir;
+        else if (y >= H[next_n]) {
+            x = X[next_n];            
+            current_n += direction;
         } 
-        else if (y < his[next]) {
+        else {
             vx = -vx;  
-            x = xis[next];
-            
+            x = X[next_n];
         }
         vy = vy - g * dt;
-        t += dt;
-
     }    
     return 0;
 }
+
 
